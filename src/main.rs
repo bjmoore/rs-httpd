b92af610d6f501d349390b70738b1355e1862fff
Benjamin Moore <benjaminjm@gmail.com>
2024-08-12 17:27:21 -0700

    add a minimal response

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 use std::{
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, BufWriter, Write},
     net::{TcpListener, TcpStream},
 };
 
 mod http;
 
-fn handle_client(stream: TcpStream) -> std::io::Result<()> {
-    // what does the serial req-reply workflow look like?
-    // when do we close out?
-    // we keep conn open and send replies until client closes the connection, OR
-    // until one of various H1.1/1,0 closure conditions
+const LOCAL_SOCK: &'static str = "127.0.0.1:8001";
+
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    // 1. build a wrapper HttpConn struct that owns the TcpStream
+    // 2. conn.get_next_request() -> HttpRequest|Error
+    // 3.  ... much logic ...
+    // 4. conn.send_reply(reply: HttpResponse) -> Ok|Error
     let mut read_buf = String::new();
-    let mut stream_reader = BufReader::new(stream);
+    let stream_reader = BufReader::new(&stream);
     let mut request: http::HttpRequest;
-    loop {
-        read_buf.clear();
-        stream_reader.read_line(&mut read_buf)?;
-        print!("{}", read_buf);
-        if read_buf == "\r\n" {
+    for line in stream_reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+        if line.is_empty() {
             break;
         }
     }
     let reply = http::HttpResponse { response_code: 200 };
+    stream.write(b"HTTP/1.1 404\r\nContent-Length: 0\r\n")?;
+    stream.flush();
     Ok(())
 }
 
 fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
-    let listener = TcpListener::bind("127.0.0.1:8001")?;
+    let listener = TcpListener::bind(LOCAL_SOCK)?;
+    println!("Now listening on {}...", LOCAL_SOCK);
     for stream in listener.incoming() {
         // these should be spun off into a thread ig
         handle_client(stream?)?;
