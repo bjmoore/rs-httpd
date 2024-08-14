16614ec5aa22505621654d74300b026a03c75f70
Benjamin Moore <benjaminjm@gmail.com>
2024-08-14 08:45:49 -0700

    Stub out http conn wrapper

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+use http::HttpConnection;
+
 mod http;
 
 const LOCAL_SOCK: &'static str = "127.0.0.1:8001";
@@ -12,19 +14,7 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     // 2. conn.get_next_request() -> HttpRequest|Error
     // 3.  ... much logic ...
     // 4. conn.send_reply(reply: HttpResponse) -> Ok|Error
-    let mut read_buf = String::new();
-    let stream_reader = BufReader::new(&stream);
-    let mut request: http::HttpRequest;
-    for line in stream_reader.lines() {
-        let line = line.unwrap();
-        println!("{}", line);
-        if line.is_empty() {
-            break;
-        }
-    }
-    let reply = http::HttpResponse { response_code: 200 };
-    stream.write(b"HTTP/1.1 404\r\nContent-Length: 0\r\n")?;
-    stream.flush();
+    let conn = HttpConnection::new(stream);
     Ok(())
 }
 
